06a69f4faa01764658a26b4f8b97cfce5758427b
benbreeden <benjamin.breeden@ontariotechu.net>
2021-02-10 12:17:25 -0500

    add p value to repo_test

--- repo_test.f90
+++ repo_test.f90
@@ -1,12 +1,14 @@
 program repo_test
 implicit none
-real :: x,y
+real :: x,y,p
 character :: key
 
 print*,'Enter a value for y'
 read*, y
 
 x = 10+y+5
+
+p = x+10
 
 print*,'Here is your value for x:',x
 
