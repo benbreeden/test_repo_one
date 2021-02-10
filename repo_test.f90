5368e07ac362d475ee3c1d45b3b766cc15666376
benbreeden <benjamin.breeden@ontariotechu.net>
2021-02-10 15:34:06 -0500

    adding q to the mix

--- repo_test.f90
+++ repo_test.f90
@@ -1,7 +1,7 @@
 program repo_test
 implicit none
 
-real :: x,y,t,p,o
+real :: x,y,t,p,o,q
 character :: key
 
 print*,'Enter a value for y'
@@ -11,6 +11,7 @@ t=5*4
 x = 10+y+5+t
 p = x+10
 o=t+x+p
+q=t-p
 
 print*,'Here is your value for x:',x,'  Here is your value for x:',o
 
